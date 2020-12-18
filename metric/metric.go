b3702cf56aa7ffbea058e60495fd62346c635780
bbapp <hello@buildbox.app>
2020-12-18 23:50:40 +0300

    v1.0.14

--- metric/metric.go
+++ metric/metric.go
@@ -9,11 +9,22 @@ import (
 	"time"
 )
 
-type serviceMetric struct {
-	Connections int `json:"connection"`				// количество открытых соединений за весь период учета
-	connectionOpen int `json:"connection_current"`	// текущее кол-во открытых соединений (+ при запрос - при ответе)
-	Queue []int `json:"queue"`						// массив соединений в очереди (не закрытых) см.выше
+type Metrics struct {
 	StateHost bbstate.StateHost
+	Connections int `json:"connection"`				// количество открытых соединений за весь период учета
+	AVG_Queue int `json:"avg_queue"` 				// среднее количество запросов в очереди
+	QTL_Queue_90 int `json:"qtl_queue_90"` 			// квантиль 90%
+	QTL_Queue_99 int `json:"qtl_queue_99"` 			// квантиль 99%
+	AVG_TPR time.Duration `json:"avg_tpr"`				// Time per request - среднее время обработки запроса
+	RPS	int `json:"rps"`							// Request per second - количество запросов в секунду
+}
+
+type serviceMetric struct {
+	Metrics
+	Stash Metrics `json:"stash"`					// карман для сохранения предыдущего значения
+
+	connectionOpen int `json:"connection_current"`	// текущее кол-во открытых соединений (+ при запрос - при ответе)
+	queue []int `json:"queue"`						// массив соединений в очереди (не закрытых) см.выше
 	mux *sync.Mutex
 	ctx context.Context
 }
@@ -22,12 +33,14 @@ type ServiceMetric interface {
 	SetState()
 	SetConnectionIncrement()
 	SetConnectionDecrement()
-	Get() serviceMetric
+	Generate() (result Metrics)
+	Get() (result Metrics)
+	Clear()
+	SaveToStash()
 	Middleware(next http.Handler) http.Handler
 }
 
 func (s *serviceMetric) SetState(){
-
 	s.mux.Lock()
 	s.StateHost.Tick()
 	s.mux.Unlock()
@@ -35,36 +48,72 @@ func (s *serviceMetric) SetState(){
 	return
 }
 
+// увеличиваем счетчик и добавляем в массив метрик
 // формируем временной ряд количества соединений
 // при начале запроса увеличиваем, при завершении уменьшаем
+// запускаем в отдельной рутине, потому что ф-ция вызывается из сервиса и не должна быть блокирующей
 func (s *serviceMetric) SetConnectionIncrement(){
 	go func() {
 		s.mux.Lock()
 		s.connectionOpen = s.connectionOpen + 1
-		s.Queue = append(s.Queue,  s.connectionOpen)
+		s.queue = append(s.queue,  s.connectionOpen)
 		s.mux.Unlock()
 	}()
 
 	return
 }
 
+// уменьшаем счетчик и добавляем в массив метрик
+// запускаем в отдельной рутине, потому что ф-ция вызывается из сервиса и не должна быть блокирующей
 func (s *serviceMetric) SetConnectionDecrement(){
 	go func() {
 		s.mux.Lock()
 		if s.connectionOpen != 0 {
 			s.connectionOpen = s.connectionOpen - 1
 		}
-		s.Queue = append(s.Queue,  s.connectionOpen)
+		s.queue = append(s.queue,  s.connectionOpen)
 		s.mux.Unlock()
 	}()
 
 	return
 }
 
-func (s *serviceMetric) Get() (result serviceMetric) {
+// сохраняем текущее значение расчитанных метрик в кармане
+func (s *serviceMetric) SaveToStash() {
+	s.mux.Lock()
+	s.Stash.RPS = s.RPS
+	s.Stash.QTL_Queue_99 = s.QTL_Queue_99
+	s.Stash.QTL_Queue_90 = s.QTL_Queue_90
+	s.Stash.AVG_TPR = s.AVG_TPR
+	s.Stash.AVG_Queue = s.AVG_Queue
+	s.mux.Unlock()
+}
+
+func (s *serviceMetric) Clear() {
+	s.mux.Lock()
+	s.Connections = 0
+	s.connectionOpen = 0
+	s.queue = []int{}
+	s.mux.Unlock()
+
+	return
+}
+
+func (s *serviceMetric) Get() (result Metrics) {
+	return s.Stash
+}
+
+func (s *serviceMetric) Generate() (result Metrics) {
+	s.SetState()	// получаю текущие метрики загрузки хоста
 	result.StateHost = s.StateHost
-	result.Connections = s.Connections
-	result.Queue = s.Queue
+	result.Connections = s.Connections	// текущее кол-во соединений
+
+	// расчитываем значения метрик
+	s.AVG_Queue = 1
+	s.AVG_TPR = 10
+	s.QTL_Queue_90 = 0
+	s.QTL_Queue_99 = 0
+	s.RPS = 100
 
 	return result
 }
@@ -84,8 +133,8 @@ func (s *serviceMetric) Middleware(next http.Handler) http.Handler {
 func New(ctx context.Context, logger *bblog.Log, interval time.Duration) (metrics ServiceMetric) {
 	m := sync.Mutex{}
 	metrics = &serviceMetric{
-		Connections: 0,
-		Queue: []int{},
+		connectionOpen: 0,
+		queue: []int{},
 		mux: &m,
 		ctx: ctx,
 	}
@@ -104,12 +153,13 @@ func RunMetricLogger(ctx context.Context, m ServiceMetric, logger *bblog.Log, in
 			case <- ctx.Done():
 				return
 			case <- ticker.C:
-				// расчитываем значения метрик
-
 				// сохраняем расчитанные значения в памяти для пинга и затираем текущие данные
 
 				// сохраняем значение метрик в лог
-				logger.Trace(m.Get())
+				m.Generate()			// сгенерировали метрики
+				m.SaveToStash()			// сохранили в карман
+				m.Clear()				// очистили объект метрик для приема новых данных
+				logger.Trace(m.Get())	// записали в лог из кармана
 
 				ticker = time.NewTicker(interval)
 			}
