fec12fadb9f90d0ac2eca9101896ec7e4822dbba
bbapp <hello@buildbox.app>
2020-12-20 20:09:08 +0300

    v1.0.22

--- log/logger.go
+++ log/logger.go
@@ -21,13 +21,13 @@ var sep = string(filepath.Separator)
 
 // структура строк лог-файла. нужна для анмаршалинга
 type LogLine struct {
-	config string `json:"config"`
-	level string `json:"level"`
-	msg interface{} `json:"msg"`
-	name string `json:"name"`
-	srv string `json:"srv"`
-	time string `json:"time"`
-	uid string `json:"uid"`
+	Config string `json:"config"`
+	Level string `json:"level"`
+	Msg interface{} `json:"msg"`
+	Name string `json:"name"`
+	Srv string `json:"srv"`
+	Time string `json:"time"`
+	Uid string `json:"uid"`
 }
 
 type Log struct {
