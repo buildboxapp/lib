61c7ed129cd06cfa538a0e5cf03d15fb52c39357
bbapp <hello@buildbox.app>
2021-08-14 21:22:09 +0300

    v1.1.44

--- models/tree.go
+++ models/tree.go
@@ -28,3 +28,23 @@ type DataTreeOut struct {
 	Incl   			[]DataTree 		 `json:"incl"`
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// метод типа Items (перемещаем структуры в карте, исходя из заявленной вложенности элементов)
+// (переделать дубль фукнции)
+func (p *DataTree) ScanSub(maps *map[string]*DataTree) {
+	if p.Sub != nil && len(p.Sub) != 0 {
+		for _, c := range p.Sub {
+			gg := *maps
+			fromP := gg[c]
+			if fromP != nil {
+				copyPolygon := *fromP
+				p.Incl = append(p.Incl, &copyPolygon)
+				delete(*maps, c)
+				copyPolygon.ScanSub(maps)
+			}
+		}
+	}
+}
